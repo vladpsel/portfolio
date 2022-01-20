a789ecb059143013e02b9a439a4a5cfb4900422e
vladpsel <vpanovwr@gmail.com>
2022-01-20 17:14:02 +0200

    update(404): update styles and scripts for mobile optimization'

--- public/js/magic.js
+++ public/js/magic.js
@@ -1,11 +1,14 @@
 function magic()
 {
   let container = document.querySelector('.magic-body');
+  let screenWidth = window.screen.width;
+
   try {
-    if (container) {
+    if (container && screenWidth >= 1366) {
       let card = container.querySelector('.not-found-card');
       let title = card.querySelector('.headline');
       let text = card.querySelector('.text-box');
+      let btn = card.querySelector('.btn-arr');
 
       container.addEventListener("mousemove", function(e) {
         let xAxis = (window.innerWidth / 2 - e.pageX)  / 10;
@@ -20,7 +23,8 @@ function magic()
           card.style.transition = 'none';
         }, 750);
           title.style.transform = "translateZ(150px)";
-          text.style.transform = "translateZ(75px)";
+          text.style.transform = "translateZ(55px)";
+          btn.style.transform = "translateZ(175px)";
       });
       // Out animation
       container.addEventListener("mouseleave", function(e){
@@ -28,14 +32,16 @@ function magic()
         card.style.transform = `rotateY(0deg) rotateX(0deg)`;
 
         title.style.transform = "translateZ(0px)";
+        text.style.transform = "translateZ(0px)";
+        btn.style.transform = "translateZ(0px)";
       });
     }
   } catch (e) {
     console.log(e);
   }
-
-
 }
+
+
 
 docReady(function(){
   magic();
