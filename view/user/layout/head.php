adf721d1110a8f35398f12aaa426b805bfb63e84
vladpsel <vpanovwr@gmail.com>
2022-02-02 14:27:55 +0200

    create(setup): create basic app

---
+++ view/user/layout/head.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="<?php echo APP_LANG; ?>" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <meta name='viewport' content='width=device-width, initial-scale=1'/>
+    <meta content='true' name='HandheldFriendly'/>
+    <meta content='width' name='MobileOptimized'/>
+    <meta content='yes' name='apple-mobile-web-app-capable'/>
+    <title>Vlad Panov | voop</title>
+    <link type="image/png" sizes="120x120" rel="icon" href="<?php echo $this->getAsset('img/favicon.png'); ?>">
+    <meta name="description" content="Влад Панов - версткальщик, Junior php developer">
+    <meta name='freelancehunt' content='edff52281dedc1b'>
+    <!-- Fonts -->
+    <link rel="stylesheet" href="<?php echo $this->getAsset('css/font.css'); ?>">
+    <!-- Styles -->
+    <link rel="stylesheet" href="<?php echo $this->getAsset('css/reset.css'); ?>">
+    <link rel="stylesheet" href="<?php echo $this->getAsset('css/style.css'); ?>">
+    <link rel="stylesheet" href="<?php echo $this->getAsset('css/media.css'); ?>">
+    <link rel="stylesheet" href="<?php echo $this->getAsset('css/animation.css'); ?>">
+</head>
+
+<body>
